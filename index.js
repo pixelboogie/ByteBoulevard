95aebce2a311749d290264a6fef4c3d5a99a1311
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-06 21:45:05 -0500

    11
    Pass convHistory into the chain as conv_history at  the point where we invoke it. Remember to make use of our formatConvHistory function. Updated the standaloneQuestionTemplate to make use of convHistory. the answerChain has access to convHistory. edited answerTemplate. It remembers conversation history. i can tell it my name, ask a question, and then it will recall my name from conv history when asked.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { StringOutputParser } from 'langchain/schema/output_parser'
 import { retriever } from '/utils/retriever'
 import { combineDocuments } from '/utils/combineDocuments'
 import { RunnablePassthrough, RunnableSequence } from "langchain/schema/runnable"
+import { formatConvHistory } from '/utils/formatConvHistory'
 
 // throw new Error("Pause execution!")
 
@@ -15,11 +16,15 @@ document.addEventListener('submit', (e) => {
 const openAIApiKey = process.env.OPENAI_API_KEY
 const llm = new ChatOpenAI({ openAIApiKey })
 
-const standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. question: {question} standalone question:'
+const standaloneQuestionTemplate = `Given some conversation history (if any) and a question, convert the question to a standalone question. 
+conversation history: {conv_history}
+question: {question} 
+standalone question:`
 const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
 
-const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided and the conversation history. Try to find the answer in the context. If the answer is not given in the context, find the answer in the conversation history if possible. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
 context: {context}
+conversation history: {conv_history}
 question: {question}
 answer: `
 const answerPrompt = PromptTemplate.fromTemplate(answerTemplate)
@@ -41,11 +46,13 @@ const chain = RunnableSequence.from([
     },
     {
         context: retrieverChain,
-        question: ({ original_input }) => original_input.question
+        question: ({ original_input }) => original_input.question,
+        conv_history: ({ original_input }) => original_input.conv_history
     },
     answerChain
 ])
 
+const convHistory = []
 
 async function progressConversation() {
     const userInput = document.getElementById('user-input')
@@ -60,8 +67,11 @@ async function progressConversation() {
     newHumanSpeechBubble.textContent = question
     chatbotConversation.scrollTop = chatbotConversation.scrollHeight
     const response = await chain.invoke({ 
-    question: question
+    question: question,
+    conv_history: formatConvHistory(convHistory)
      })
+    convHistory.push(question)
+    convHistory.push(response)
 
     // add AI message
     const newAiSpeechBubble = document.createElement('div')
