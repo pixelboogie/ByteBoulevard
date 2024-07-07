6ffd30245347ae7d9f6c01098c95c945c40b35f7
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-06 21:43:02 -0500

    5
    Adding the first chain, reduced the first quesiton into a standalone question

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+import { ChatOpenAI } from "langchain/chat_models/openai"
+import { PromptTemplate } from "langchain/prompts"
+
+// throw new Error("Pause execution!")
+
 document.addEventListener('submit', (e) => {
     e.preventDefault()
     progressConversation()
 })
 
 const openAIApiKey = process.env.OPENAI_API_KEY
+const llm = new ChatOpenAI({ openAIApiKey })
+
+
+// A string holding the phrasing of the prompt
+const standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. question: {question} standalone question:'
+
+// A prompt created using PromptTemplate and the fromTemplate method
+const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
+
+// Take the standaloneQuestionPrompt and PIPE the model
+const standaloneQuestionChain = standaloneQuestionPrompt.pipe(llm)
+
+// Await the response when you INVOKE the chain. 
+// Remember to pass in a question.
+const response = await standaloneQuestionChain.invoke({ 
+    question: 'What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.'
+     })
+
+console.log(response)
 
 async function progressConversation() {
     const userInput = document.getElementById('user-input')
