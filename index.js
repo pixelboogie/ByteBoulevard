2894d75a2ddaa5a379b190533c8093aa9f943a9b
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-06 21:44:25 -0500

    9
    Set up a RunnableSequence so that the standaloneQuestionPrompt, passes the standalone question to the retriever, and the retriever passes the combined docs as context to the answerPrompt. the answerPrompt should also has access to the original question. it should be a conversational answer to our question in the console.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import { ChatOpenAI } from "langchain/chat_models/openai"
 import { PromptTemplate } from "langchain/prompts"
 import { StringOutputParser } from 'langchain/schema/output_parser'
 import { retriever } from '/utils/retriever'
+import { combineDocuments } from '/utils/combineDocuments'
+import { RunnablePassthrough, RunnableSequence } from "langchain/schema/runnable"
 
-throw new Error("Pause execution!")
+// throw new Error("Pause execution!")
 
 document.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -11,19 +13,39 @@ document.addEventListener('submit', (e) => {
 })
 
 const openAIApiKey = process.env.OPENAI_API_KEY
-
-const embeddings = new OpenAIEmbeddings({ openAIApiKey })
 const llm = new ChatOpenAI({ openAIApiKey })
 
 const standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. question: {question} standalone question:'
-
 const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
 
-const chain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser())
-.pipe(retriever)
+const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+context: {context}
+question: {question}
+answer: `
+const answerPrompt = PromptTemplate.fromTemplate(answerTemplate)
 
-// Await the response when you INVOKE the chain. 
-// Remember to pass in a question.
+const standaloneQuestionChain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser())
+const retrieverChain = RunnableSequence.from([
+    prevResult => prevResult.standalone_question,
+    retriever,
+    combineDocuments
+])
+const answerChain = answerPrompt
+    .pipe(llm)
+    .pipe(new StringOutputParser())
+
+const chain = RunnableSequence.from([
+    {
+        standalone_question: standaloneQuestionChain,
+        original_input: new RunnablePassthrough()
+    },
+    {
+        context: retrieverChain,
+        question: ({ original_input }) => original_input.question
+    },
+    answerChain
+])
+
 const response = await chain.invoke({ 
     question: 'What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.'
      })
