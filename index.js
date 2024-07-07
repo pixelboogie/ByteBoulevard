6039a1d9b009768801de6a1462242bbf9ab460e5
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-06 21:41:53 -0500

    2
    Splits the text into chunks

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 try {
   const result = await fetch('scrimba-info.txt')
   const text = await result.text()
+  
+  const splitter = new RecursiveCharacterTextSplitter()
+  
+  const output = await splitter.createDocuments([text])
+  console.log(output)
 } catch (err) {
   console.log(err)
 }
