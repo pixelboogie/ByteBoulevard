a76f67b387b63e9b4a38093c06449a320698c9b9
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-06 21:43:42 -0500

    7
    output parser, chained everything together, getting all 4 chunks together

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { ChatOpenAI } from "langchain/chat_models/openai"
 import { PromptTemplate } from "langchain/prompts"
-
 import { SupabaseVectorStore } from "langchain/vectorstores/supabase"
 import { OpenAIEmbeddings } from "langchain/embeddings/openai"
 import { createClient } from "@supabase/supabase-js"
+import { StringOutputParser } from 'langchain/schema/output_parser'
 
-// throw new Error("Pause execution!")
+throw new Error("Pause execution!")
 
 document.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -37,18 +37,17 @@ const standaloneQuestionTemplate = 'Given a question, convert it to a standalone
 const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
 
 // Take the standaloneQuestionPrompt and PIPE the model
-const standaloneQuestionChain = standaloneQuestionPrompt.pipe(llm)
+const chain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser())
+.pipe(retriever)
 
 // Await the response when you INVOKE the chain. 
 // Remember to pass in a question.
-const response = await standaloneQuestionChain.invoke({ 
+const response = await chain.invoke({ 
     question: 'What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.'
      })
      
-const response2 = await retriever.invoke('Will Scrimba work on an old laptop?')
 
-// console.log(response)
-console.log(response2)
+console.log(response)
 
 async function progressConversation() {
     const userInput = document.getElementById('user-input')
