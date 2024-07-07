34676b01a67b3cde056fa6cc2dfd2f6b024e9341
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-06 21:42:16 -0500

    3
    controlling the chunk size, seperators, chunkOverlap, add api keys, add supabase client, supabase vector store, get's the data into ebeddings and loaded into the database

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseVectorStore } from 'langchain/vectorstores/supabase'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 
+// @supabase/supabase-js
 try {
   const result = await fetch('scrimba-info.txt')
   const text = await result.text()
   
-  const splitter = new RecursiveCharacterTextSplitter()
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 50,
+    separators: ['\n\n', '\n', ' ', ''] // default setting
+  })
   
   const output = await splitter.createDocuments([text])
-  console.log(output)
+  
+    const sbApiKey = process.env.SUPABASE_API_KEY
+    const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT
+    const openAIApiKey = process.env.OPENAI_API_KEY
+    
+    const client = createClient(sbUrl, sbApiKey)
+    
+    await SupabaseVectorStore.fromDocuments(
+        output,
+        new OpenAIEmbeddings({ openAIApiKey }),
+        {
+            client,
+            tableName: 'documents',
+        }
+    )
 } catch (err) {
   console.log(err)
 }
