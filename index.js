044351b996a9736fb7d33c919fb48f25af575fc0
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2024-07-06 21:42:42 -0500

    4
    starter code for the app

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
-import { createClient } from '@supabase/supabase-js'
-import { SupabaseVectorStore } from 'langchain/vectorstores/supabase'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    progressConversation()
+})
 
-// @supabase/supabase-js
-try {
-  const result = await fetch('scrimba-info.txt')
-  const text = await result.text()
-  
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 500,
-    chunkOverlap: 50,
-    separators: ['\n\n', '\n', ' ', ''] // default setting
-  })
-  
-  const output = await splitter.createDocuments([text])
-  
-    const sbApiKey = process.env.SUPABASE_API_KEY
-    const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT
-    const openAIApiKey = process.env.OPENAI_API_KEY
-    
-    const client = createClient(sbUrl, sbApiKey)
-    
-    await SupabaseVectorStore.fromDocuments(
-        output,
-        new OpenAIEmbeddings({ openAIApiKey }),
-        {
-            client,
-            tableName: 'documents',
-        }
-    )
-} catch (err) {
-  console.log(err)
+const openAIApiKey = process.env.OPENAI_API_KEY
+
+async function progressConversation() {
+    const userInput = document.getElementById('user-input')
+    const chatbotConversation = document.getElementById('chatbot-conversation-container')
+    const question = userInput.value
+    userInput.value = ''
+
+    // add human message
+    const newHumanSpeechBubble = document.createElement('div')
+    newHumanSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newHumanSpeechBubble)
+    newHumanSpeechBubble.textContent = question
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+
+    // add AI message
+    const newAiSpeechBubble = document.createElement('div')
+    newAiSpeechBubble.classList.add('speech', 'speech-ai')
+    chatbotConversation.appendChild(newAiSpeechBubble)
+    newAiSpeechBubble.textContent = result
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
 }
